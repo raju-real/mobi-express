f75ae4a65ee1ade9b7544eadc5547b5b05b916f8
raju-bigm <mkraju.bigm@gmail.com>
2021-10-31 15:57:42 +0600

    promotion title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,7 @@
                 <div class="row">
                     <div class="col-lg-8 col-12">
                         <div class="slider_content slider_c_three color_white">
-                            <h1>{{ $promotion->name }}</h1>
+                            <h1 style="color: red;font-weight: bold;">{{ $promotion->name }}</h1>
                             @if(sizeof($promotion->products) > 0)
                                 <a class="button" 
                                 href="{{ route('campaign-products',$promotion->slug) }}">Shop Now</a>
